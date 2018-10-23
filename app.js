0e05bd9b3f4d9940b22a1a579c46ea5894cf1d46
Unknown <luigiaiello97@gmail.com>
2018-10-23 15:44:12 +0200

    Add connection with mongo DB

--- app.js
+++ app.js
@@ -1,8 +1,26 @@
 const express = require('express');
 const path = require('path');
+const mongoose = require('mongoose');
+
+mongoose.connect("mongodb://localhost/nodekb", { useNewUrlParser: true });
+
+let db = mongoose.connection;
+
+// Check connection
+db.once('open', function() {
+  console.log('Connected to MongoDB');
+});
+
+// Check for DB errors
+db.on('error', function(err) {
+  console.log(err);
+});
 
 // Init App
 const app = express();
+
+// Bring in Models
+let Article = require('./models/article');
 
 // Load View Engine
 app.set('views', path.join(__dirname, 'views'));
@@ -10,28 +28,15 @@ app.set('view engine', 'pug')
 
 // Home Route
 app.get('/', function(req, res){
-  let articles = [
-    {
-      id: 1,
-      title: 'Article One',
-      author: 'Brad Traversy',
-      body: 'This is article one'
-    },
-    {
-      id: 2,
-      title: 'Article Two',
-      author: 'John Doe',
-      body: 'This is article two'
-    },
-    {
-      id: 3,
-      title: 'Article Three',
-      author: 'Brad Traversy',
-      body: 'This is article three'
+  Article.find({}, function(err, articles){
+    if(err){
+      console.log(err);
+    } else {
+      res.render('index', {
+        title: 'Articles',
+        articles: articles
+      });
     }
-  ];
-  res.render('index', {
-    title: 'Hello'
   });
 });
 
